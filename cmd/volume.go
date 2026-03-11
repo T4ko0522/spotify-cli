dc017a859d0bc2a94b6c97260607217b120fb399
T4ko0522 <tako.renraku1@gmail.com>
2026-03-11 20:49:21 +0900

    feat: ✨ volumeコマンドの引数処理を修正
    - 引数が指定されていない場合にTUIを使用してボリュームを表示するように変更しました。また、引数が指定された場合のボリューム設定処理を追加しました。

--- cmd/volume.go
+++ cmd/volume.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"github.com/spf13/cobra"
+	"github.com/T4ko0522/spotify-cli/internal/tui"
 )
 
 var volumeCmd = &cobra.Command{
@@ -13,15 +14,10 @@ var volumeCmd = &cobra.Command{
 	Short:   "Get or set playback volume",
 	Args:  cobra.MaximumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		ctx := cmd.Context()
 		if len(args) == 0 {
-			state, err := spotifyPlayer.PlayerState(ctx)
-			if err != nil {
-				return err
-			}
-			fmt.Printf("Volume: %d%%\n", state.Device.Volume)
-			return nil
+			return tui.RunVolume(spotifyPlayer)
 		}
+		ctx := cmd.Context()
 		vol, err := strconv.Atoi(args[0])
 		if err != nil || vol < 0 || vol > 100 {
 			return fmt.Errorf("volume must be a number between 0 and 100")
